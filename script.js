a2959cf11f5116430e2d9906935524c1361f9e10
AnimationT <86012532+AnimationT@users.noreply.github.com>
2025-12-07 21:54:25 +0000

    everything(might add some more, but coded everything so far on jsfiddle and then pasted everything here)

---
+++ script.js
@@ -0,0 +1,54 @@
+var onclicks = {};
+
+var treeNum = 0;
+
+
+class Tree {
+  constructor (height = 100, type = "regular") {
+    this.height = height;
+    this.type = type;
+  }
+  summon () {
+    treeNum++;
+  if (this.type === "regular") {
+    $("#trees").append("<img id=" + treeNum + " src="+"https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Trees-PNG-Clipart/Tree_Cartoon_PNG_Clip_Art_Image.png?m=1629818656"+">");
+  } else if (this.type === "pine") {
+    $("#trees").append("<img id=" + treeNum + " src="+"https://www.onlygfx.com/wp-content/uploads/2021/08/pine-tree-drawing-5061.svg"+">");
+  }
+  $("#delete").append("<option id="+"tree"+treeNum+" value="+treeNum+">Tree"+treeNum+"</option>");
+  $("#"+treeNum).css({
+      "height": this.height,
+      "width": this.height
+    });
+  }
+}
+var treeHeight = parseInt($("#height").val());
+var treeType = $("#type").val();
+var deleteValue;
+var mainOnclick;
+
+setInterval(function () {
+  treeHeight = parseInt($("#height").val());
+  treeType = $("#type").val();
+  deleteValue = $("#delete").val();
+  mainOnclick = () => onclicks["onclick" + deleteValue];
+  mainOnclick = function () {
+    if (treeNum > 0) {
+      $("#"+deleteValue).remove();
+      $("#tree"+treeNum).remove();
+    treeNum--;
+    } else {
+
+    }
+  }
+});
+
+setInterval(function () {
+  $("#deleteButton").attr("onclick", "mainOnclick();");
+}, 10);
+
+
+function create(height, type) {
+  let tree = new Tree(height, type);
+  tree.summon();
+}
